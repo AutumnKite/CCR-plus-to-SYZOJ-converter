e1db8d20ad1f431e64a09bbd25b28a94dc76c362
AutumnKite <1790397194@qq.com>
2020-06-18 21:11:32 +0800

    init

---
+++ README.md
@@ -0,0 +1,65 @@
+# 从 CCR Plus 到 SYZOJ 的数据配置格式转换器
+
+[CCR Plus 项目地址](https://github.com/sxyzccr/CCR-Plus)
+
+[SYZOJ 项目地址](https://github.com/syzoj/syzoj)
+
+## 使用方法
+
+在同目录下放置 CCR Plus 的数据配置文件 `.prb`。
+
+在命令行中输入命令
+
+```plain
+g++ work.cpp tinystr.cpp tinyxml.cpp tinyxmlerror.cpp tinyxmlparser.cpp -o work -std=c++11 -O2
+```
+
+直接运行生成的可执行文件 `work.exe`（Windows） / `work`（Linux）即可得到 SYZOJ 的数据配置文件 `data.yml`。
+
+## 输出信息
+
+若程序输出
+
+```plain
+Failed to load file ".prb".
+```
+
+则说明 `.prb` 文件不存在或 xml 语法有误。
+
+若输出
+
+```plain
+No subtasks.
+```
+
+则说明不存在任何 subtask 或 `.prb` 文件中对 subtask 的描述有误。
+
+```plain
+No test cases.
+```
+
+则意味着存在 subtask 但所有 subtask 中都不存在测试点或测试点描述有误。
+
+```plain
+Cannot recognize the common part of the filename.
+```
+
+表示现在的测试数据文件名无法转换，具体参见注意事项中的第 3 条。
+
+以上情况皆为一些无法正常转换的情况。
+
+若程序输出
+
+```plain
+Success.
+```
+
+则说明转换成功。
+
+文件中的 `.prb` 和 `data.yml` 即为一组转换成功的例子。
+
+## 注意事项
+
+1. 目前只支持传统题的配置。
+2. 由于 CCR Plus 的 Special Judge 只需要可执行文件，而 SYZOJ 的 Special Judge 需要源代码，故无法自动配置。
+3. **SYZOJ 的配置文件中，同组数据输入输出文件名的 `#` 是用同一字符串代替的，所以输入输出文件名的格式限制较大，可能存在无法转换的情况。**
---
+++ work.cpp
@@ -0,0 +1,121 @@
+#include "tinyxml.h"
+#include "tinystr.h"
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+int LCP(const std::vector<std::string> &a) {
+	if (a.empty()) {
+		return -1;
+	}
+	for (int len = 0; ; ++len) {
+		for (const std::string &v : a) {
+			if (len >= (int)v.size() || v[len] != a[0][len]) {
+				return len;
+			}
+		}
+	}
+	return -1;
+}
+
+int LCS(const std::vector<std::string> &a) {
+	auto t = a;
+	for (std::string &v : t) {
+		std::reverse(v.begin(), v.end());
+	}
+	return LCP(t);
+}
+
+int main() {
+	std::ofstream fout("data.yml");
+
+	TiXmlDocument prb;
+	if (!prb.LoadFile(".prb")) {
+		std::cout << "Failed to load file \".prb\"." << std::endl;
+		return 0;
+	}
+
+	TiXmlElement *root = prb.FirstChildElement();
+	if (root == NULL || root->ValueTStr() != "problem") {
+		std::cout << "No subtasks." << std::endl;
+		return 0;
+	}
+
+	std::vector<std::string> in, out;
+	std::vector<std::vector<int>> id;
+	std::vector<std::string> sc;
+	for (TiXmlElement *dd = root->FirstChildElement(); dd != NULL; dd = dd->NextSiblingElement()) {
+		if (dd->ValueTStr() != "task") {
+			continue;
+		}
+		for (TiXmlElement *d = dd->FirstChildElement(); d != NULL; d = d->NextSiblingElement()) {
+			if (d->ValueTStr() != "subtask") {
+				continue;
+			}
+			std::string score = d->Attribute("score");
+			sc.push_back(score);
+			id.push_back({});
+			for (TiXmlElement *c = d->FirstChildElement(); c != NULL; c = c->NextSiblingElement()) {
+				if (c->ValueTStr() != "point") {
+					continue;
+				}
+				id.back().push_back(in.size());
+				in.push_back(c->Attribute("in"));
+				out.push_back(c->Attribute("out"));
+			}
+		}
+	}
+
+	if (sc.empty()) {
+		std::cout << "No subtasks." << std::endl;
+		return 0;
+	}
+	if (in.empty()) {
+		std::cout << "No test cases." << std::endl;
+		return 0;
+	}
+
+	int inp = LCP(in), ins = LCS(in), inl = 1e9;
+	int outp = LCP(out), outs = LCS(out), outl = 1e9;
+	for (const std::string &v : in) {
+		inl = std::min(inl, (int)v.size());
+	}
+	for (const std::string &v : out) {
+		outl = std::min(outl, (int)v.size());
+	}
+	inp = std::min(inp, inl - 1), ins = std::min(ins, inl - 1 - inp);
+	outp = std::min(outp, outl - 1), outs = std::min(outs, outl - 1 - outp);
+
+	std::string inform = in[0].substr(0, inp) + "#" + in[0].substr((int)in[0].size() - ins, ins);
+	std::string outform = out[0].substr(0, outp) + "#" + out[0].substr((int)out[0].size() - outs, outs);
+
+	for (int i = 0; i < (int)in.size(); ++i) {
+		in[i] = in[i].substr(inp, (int)in[i].size() - inp - ins);
+		out[i] = out[i].substr(outp, (int)out[i].size() - outp - outs);
+		if (in[i] != out[i]) {
+			std::cout << "Cannot recognize the common part of the filename." << std::endl;
+			return 0;
+		}
+	}
+
+	fout << "subtasks: \n";
+	for (int i = 0; i < (int)sc.size(); ++i) {
+		fout << "  - score: " << sc[i] << "\n";
+		fout << "    type: min\n";
+		fout << "    cases: [";
+		for (int j = 0; j < (int)id[i].size(); ++j) {
+			if (j) {
+				fout << ", ";
+			}
+			fout << "\'" << in[id[i][j]] << "\'";
+		}
+		fout << "]\n";
+	}
+	fout << "\n";
+	fout << "inputFile: \'" << inform << "\'\n";
+	fout << "outputFile: \'" << outform << "\'\n";
+	std::cout << "Success." << std::endl;
+	return 0;
+}
